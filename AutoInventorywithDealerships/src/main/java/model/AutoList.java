9dce86552cb258195a6ce4d49ffe8595fa117a72
pjtech <pjtechproductions@gmail.com>
2022-10-09 23:20:36 -0500

    First commit.

---
+++ AutoInventorywithDealerships/src/main/java/model/AutoList.java
@@ -0,0 +1,82 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @authors peterjohnson & logan kennebeck - pmjohnson5 & ljkennebeck1
+ * CIS175-Fall 2022
+ * October 8, 2022
+ */
+
+@Entity
+@Table(name = "autos")
+public class AutoList {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private int id;
+	@Column(name = "MAKE")
+	private String make;
+	@Column(name = "MODEL")
+	private String model;
+	@Column(name = "YEAR")
+	private String year;
+	
+	public AutoList() {
+		super();		
+	}
+	
+	public AutoList(String make, String model, String year) {
+		super();
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public String getYear() {
+		return year;
+	}
+	
+	public void setYear(String year) {
+		this.year = year;
+	}
+	
+	public String returnAlbumDetails() {
+		return make + " : " + model + " : " + year;
+	}
+	
+}
+
+	
+	
+
+
