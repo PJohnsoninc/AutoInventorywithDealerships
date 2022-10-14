3a8b1fdfa7699350dfc43c28b78dacfcc3fd7a2c
pjtech <pjtechproductions@gmail.com>
2022-10-13 23:36:49 -0500

    Added file. First commit.

---
+++ AutoInventorywithDealerships/src/main/java/controller/CreateNewDealershipServlet.java
@@ -0,0 +1,84 @@
+package controller;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.AutoBrands;
+import model.AutoList;
+import model.Dealership;
+
+/**
+ * Servlet implementation class CreateNewDealershipServlet
+ */
+@WebServlet("/createNewDealershipServlet")
+public class CreateNewDealershipServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateNewDealershipServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		AutoListHelper alh = new AutoListHelper();
+		String dealershipName = request.getParameter("dealershipName");
+		System.out.println("Dealership Name: " + dealershipName);
+		
+		String month = request.getParameter("month");
+		String day = request.getParameter("day");
+		String year = request.getParameter("year");
+		
+		LocalDate ld;
+		try {
+			ld = LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+		}catch (NumberFormatException ex){
+			ld = LocalDate.now();
+		}
+		
+		String[] selectedAutos = request.getParameterValues("allAutosToAdd");
+		List<AutoList> selectedAutosInList = new ArrayList<AutoList>();
+		
+		if(selectedAutos != null && selectedAutos.length > 0){
+			for(int i = 0; i<selectedAutos.length; i++) {
+				System.out.println(selectedAutos[i]);
+				AutoList c = alh.searchById(Integer.parseInt(selectedAutos[i]));
+				selectedAutosInList.add(c);
+			}
+		}
+		
+		
+		Dealership cld = new Dealership(dealershipName, ld);
+		cld.setListOfVehicles(selectedAutosInList);
+		DealershipHelper clh = new DealershipHelper();
+		clh.insertDealership(cld);
+		
+		System.out.println("Success!");
+		System.out.println(cld.toString());
+		
+		getServletContext().getRequestDispatcher("/viewAllDealershipsServlet").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
