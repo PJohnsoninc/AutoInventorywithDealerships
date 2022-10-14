f4fd5bc578ab078c3021da904c434431b4b6b056
pjtech <pjtechproductions@gmail.com>
2022-10-13 23:20:14 -0500

    Added file.  First commit.

---
+++ AutoInventorywithDealerships/src/main/java/controller/LocalDateAttributeConverter.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * @authors peterjohnson & logan kennebeck - pmjohnson5 & ljkennebeck1
+ * CIS175-Fall 2022
+ * October 11, 2022
+ */
+
+@Converter(autoApply = true)
+public class LocalDateAttributeConverter implements AttributeConverter<LocalDate, Date> {
+
+	@Override
+	public Date convertToDatabaseColumn(LocalDate attribute) {
+		// TODO Auto-generated method stub
+		return (attribute == null ? null: Date.valueOf(attribute));
+	}
+
+	@Override
+	public LocalDate convertToEntityAttribute(Date dbData) {
+		// TODO Auto-generated method stub
+		return (dbData == null ? null: dbData.toLocalDate());
+	}
+
+}
+
